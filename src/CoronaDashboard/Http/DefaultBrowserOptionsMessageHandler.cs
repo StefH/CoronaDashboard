deb6c828a46a6b1c9b0cbd3c9e3e21f2c42840ce
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-11-09 17:52:18 +0100

    DefaultBrowserOptionsMessageHandler

---
+++ src/CoronaDashboard/Http/DefaultBrowserOptionsMessageHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
+
+namespace CoronaDashboard.Http
+{
+    public sealed class DefaultBrowserOptionsMessageHandler : DelegatingHandler
+    {
+        public BrowserRequestCache DefaultBrowserRequestCache { get; set; }
+        public BrowserRequestCredentials DefaultBrowserRequestCredentials { get; set; }
+        public BrowserRequestMode DefaultBrowserRequestMode { get; set; }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            // Get the existing options to not override them if set explicitly
+            IDictionary<string, object> existingProperties = null;
+            if (request.Properties.TryGetValue("WebAssemblyFetchOptions", out object fetchOptions))
+            {
+                existingProperties = (IDictionary<string, object>)fetchOptions;
+            }
+
+            if (existingProperties?.ContainsKey("cache") != true)
+            {
+                request.SetBrowserRequestCache(DefaultBrowserRequestCache);
+            }
+
+            if (existingProperties?.ContainsKey("credentials") != true)
+            {
+                request.SetBrowserRequestCredentials(DefaultBrowserRequestCredentials);
+            }
+
+            if (existingProperties?.ContainsKey("mode") != true)
+            {
+                request.SetBrowserRequestMode(DefaultBrowserRequestMode);
+            }
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
